247bd3251f31e3870e1fa00722c84fca7f49933f
Kieran <kieranknowles11@hotmail.co.uk>
2024-11-10 19:40:41 +0000

    Basic rendering

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,26 @@
-import { BackendAPI } from "trilium/backend";
+import { FrontendAPI } from "trilium/frontend";
 
 // The API object is available in the global scope
-declare const api: BackendAPI;
+declare const api: FrontendAPI;
+
+interface Data {
+    [key: string]: string
+}
+
+function displayValue(query: string, value: string | null) {
+    if (value === null) {
+        $(query).css('color', 'red')
+    }
+    $(query).text(value || 'Not set')
+}
+
+async function render(): Promise<void> {
+    const dataChild = api.getActiveContextNote().children[0];
+    const data = await api.getNote(dataChild)
+        .then(note => note.getBlob())
+        .then(blob => blob.content)
+    const json: Data = JSON.parse(data)
+
+    displayValue('#name', json['name'])
+}
+render()
