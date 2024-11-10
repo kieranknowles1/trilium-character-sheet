eb9b673fd124db0631b66a0fadc101507c57e691
Kieran <kieranknowles11@hotmail.co.uk>
2024-11-10 20:16:46 +0000

    Add demo

--- tpack.config.js
+++ tpack.config.js
@@ -1,3 +1,21 @@
+function createRenderNote(title, src, isTemplate) {
+    return {
+        title,
+        type: "render",
+        attributes: isTemplate ? {
+            "~renderNote": "static/main.html", "#template": ""
+        } : {
+            "~renderNote": "static/main.html"
+        },
+        children: [
+            {
+                title: "Data",
+                file: src
+            }
+        ]
+    }
+}
+
 module.exports = {
     output: "dist.zip",
     notes: {
@@ -5,29 +23,19 @@ module.exports = {
         file: "readme.md",
         children: [
             {
+                // Data that the template renders from
                 file: "static/main.html",
                 title: "HTML",
                 children: [
                     {
+                        // This script is executed when opening the note
                         file: "src/main.js",
                         env: "frontend",
                     }
                 ]
             },
-            {
-                title: "Character Sheet",
-                type: "render",
-                attributes: {
-                    "~renderNote": "static/main.html",
-                    "#template": true,
-                },
-                children: [
-                    {
-                        file: "static/sheet.json",
-                        title: "Data",
-                    }
-                ]
-            }
+            createRenderNote("Character Sheet", "static/sheet.json", true),
+            createRenderNote("Demo", "static/demo.json", false),
         ]
     }
 }
